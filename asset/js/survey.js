9e3db6c05e7ff130d73d5e6d7c942009ca735058
hester.hung <hester.hung@zensis.com>
2024-10-17 17:25:14 +0800

    + created first survey page

---
+++ asset/js/survey.js
@@ -0,0 +1,4 @@
+document.querySelector('.next-btn').addEventListener('click', function () {
+    alert('Next button clicked!');
+    // You can add any additional actions here, like form submission
+});
